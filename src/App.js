d61691dfcdb85a7a88382ba123e5cb3de523b251
Summer S <snowykittin@gmail.com>
2022-11-07 14:22:01 -0500

    music player lab

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import RecipeSearch from "./components/RecipeSearch";
 import ReactAnimation from "./components/ReactAnimation";
 import Map from "./components/Map/Map";
+import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 //map work pls
 import Inventory from "./components/inventory/Inventory";
 
 
 export default class App extends React.Component{
   state = {
-    pages: [<RecipeSearch />, <ReactAnimation />, <Map />, <Inventory />],
+    pages: [<RecipeSearch />, <ReactAnimation />, <Map />, <Inventory />, <MusicPlayer />],
     curPage: 0
   };
   
   render(){
     return (
       <div className="App">
-        <h1>Welcome to React</h1>
+        <h1>N320 React Projects</h1>
         <div>
           <button onClick={() => {
             this.swapProject(0);
@@ -37,6 +38,11 @@ export default class App extends React.Component{
             this.swapProject(3);
             }}>
               Inventory Bag
+          </button>
+          <button onClick={() => {
+            this.swapProject(4);
+            }}>
+              Music Player
           </button>
         </div>
         {this.state.pages[this.state.curPage]}
